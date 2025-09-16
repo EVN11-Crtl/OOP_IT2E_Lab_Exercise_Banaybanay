75e6947307748dd4f50274b59a967914bf8195b9
EVN • CREATIONS <145964977+EVN11-Crtl@users.noreply.github.com>
2025-09-16 10:48:50 +0800

    Delete EXCER_3 directory

--- EXCER_3/Shape.java
+++
@@ -1,9 +0,0 @@
-public class Shape {
-    public double calculateArea() {
-        return 0.0; 
-    }
-
-    public void displayArea() {
-        System.out.println(getClass().getSimpleName() + " Area: " + calculateArea());
-    }
-}
